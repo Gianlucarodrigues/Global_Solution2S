34f3a853b4ad2c47201a6bd8fd1a6bec99024e4a
Gianluca <giancordeiro10@gmail.com>
2025-11-22 17:19:23 -0300

    Atualização de CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SkillMatch from "./pages/SkillMatch";
 import "./styles/App.css";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/skillmatch" element={<SkillMatch />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/skillmatch" element={<SkillMatch />} />
+      </Routes>
+
+      <Footer /> 
+    </>
   );
 }
+
--- src/Main.jsx
+++ src/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
